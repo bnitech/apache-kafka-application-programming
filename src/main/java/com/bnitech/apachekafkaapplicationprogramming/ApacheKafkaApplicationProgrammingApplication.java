1740c724a27bef0d457399f81bd55c50c6d07bba
Dongbin <bnitech88@gmail.com>
2022-08-12 15:53:13 +0900

    init project

---
+++ src/main/java/com/bnitech/apachekafkaapplicationprogramming/ApacheKafkaApplicationProgrammingApplication.java
@@ -0,0 +1,13 @@
+package com.bnitech.apachekafkaapplicationprogramming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApacheKafkaApplicationProgrammingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApacheKafkaApplicationProgrammingApplication.class, args);
+    }
+
+}
