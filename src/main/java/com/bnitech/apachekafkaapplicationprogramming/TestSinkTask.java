50627da4451db424a6271f88f2bd530939cb15db
Dongbin <bnitech88@gmail.com>
2022-08-14 01:28:08 +0900

    [dongbin] add sink connector

---
+++ src/main/java/com/bnitech/apachekafkaapplicationprogramming/TestSinkTask.java
@@ -0,0 +1,30 @@
+package com.bnitech.apachekafkaapplicationprogramming;
+
+import org.apache.kafka.connect.sink.SinkRecord;
+import org.apache.kafka.connect.sink.SinkTask;
+
+import java.util.Collection;
+import java.util.Map;
+
+public class TestSinkTask extends SinkTask {
+
+    @Override
+    public String version() {
+        return null;
+    }
+
+    @Override
+    public void start(Map<String, String> props) {
+
+    }
+
+    @Override
+    public void put(Collection<SinkRecord> records) {
+
+    }
+
+    @Override
+    public void stop() {
+
+    }
+}
