5aa5adfb0dc271701e580f0a8085c8968277d7a7
Dongbin <bnitech88@gmail.com>
2022-08-14 03:20:03 +0900

    [dongbin] add record listener

--- src/main/java/com/bnitech/apachekafkaapplicationprogramming/ApacheKafkaApplicationProgrammingApplication.java
+++ src/main/java/com/bnitech/apachekafkaapplicationprogramming/ApacheKafkaApplicationProgrammingApplication.java
@@ -1,29 +1,13 @@
 package com.bnitech.apachekafkaapplicationprogramming;
 
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.kafka.core.KafkaTemplate;
 
 @SpringBootApplication
-@RequiredArgsConstructor
-public class ApacheKafkaApplicationProgrammingApplication implements CommandLineRunner {
-
-    private static String TOPIC_NAME = "test";
-
-    private final KafkaTemplate<Integer, String> template;
+public class ApacheKafkaApplicationProgrammingApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ApacheKafkaApplicationProgrammingApplication.class, args);
     }
 
-    @Override
-    public void run(String... args) throws Exception {
-        for (int i = 0; i < 10; i++) {
-            template.send(TOPIC_NAME, "test" + i);
-        }
-        System.exit(0);
-    }
 }
